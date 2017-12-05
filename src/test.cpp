dc2d09d20c936ed7255190c81f0e0be14cfd0e3b
ilivanov <ya.ilivanov@yandex.ru>
2017-12-05 21:33:47 +0300

    test 4

--- src/test.cpp
+++ src/test.cpp
@@ -1,11 +1,11 @@
-#define BOOST_TEST_MODULE test_version
+#define BOOST_TEST_MODULE TestVersion
 
 #include <boost/test/unit_test.hpp>
 #include "../bin/version.h"
 
-BOOST_AUTO_TEST_SUITE(test_version)
+BOOST_AUTO_TEST_SUITE(test_suite)
 
-BOOST_AUTO_TEST_CASE(test_valid_version)
+BOOST_AUTO_TEST_CASE(test_version)
 {
   BOOST_CHECK(version() > 0);
 }
