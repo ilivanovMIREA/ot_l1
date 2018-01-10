a9ef87293d5d217f591f773d7c955d45bdb60842
ilivanov <ya.ilivanov@yandex.ru>
2018-01-10 17:45:26 +0300

    test after new year

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,10 @@
 int main(){
   auto logger = spdlog::stdout_logger_mt("console");
   logger->info("version {} was 1 started", version());
+  
+
   std::cout << "Hello, World!";
+  
+
   return 0;
 }
